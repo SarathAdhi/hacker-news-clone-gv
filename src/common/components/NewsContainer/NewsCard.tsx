ae625539ba54a39f89cefbe82d8e4142e1ca8b6f
SarathAdhi <s.sarathadhithya@gmail.com>
2023-01-16 11:43:40 +0530

    ui changes in `NewsCard`

--- src/common/components/NewsContainer/NewsCard.tsx
+++ src/common/components/NewsContainer/NewsCard.tsx
@@ -1,9 +1,12 @@
-import { Paper, Tooltip, Typography } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import Tooltip from "@mui/material/Tooltip";
 import React from "react";
 import { News } from "../../types/news";
 import moment from "moment";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 
 type Props = {
   number: number;
@@ -23,62 +26,49 @@ export const NewsCard: React.FC<Props> = ({
   num_comments,
 }) => {
   return (
-    <Paper
-      elevation={1}
-      sx={
-        {
-          // "&:hover": {
-          //   boxShadow: 5,
-          // },
-        }
-      }
-      className="p-3 flex items-cente"
-    >
-      <Typography variant="h5" className="text-gray-400">
-        {number}. &nbsp;
+    <Paper elevation={1} className="p-3 flex gap-2">
+      <Typography variant="h5" className="flex text-gray-400">
+        {number}.
       </Typography>
 
-      <div className="w-full flex justify-between gap-5">
-        <div>
+      <div className="w-full flex items-start justify-between gap-10">
+        <div className="flex flex-col justify-between h-full">
           <Typography variant="h5">{title || "[ No Title ]"}</Typography>
 
           {comment_text && (
             <p
-              className="font-medium"
+              className="font-medium text-sm md:text-base"
               dangerouslySetInnerHTML={{ __html: comment_text }}
             />
           )}
 
-          <Typography variant="body2" className="text-gray-400">
-            {points} points by{" "}
-            <a
-              href={`http://hn.algolia.com/api/v1/users/${author}`}
-              className="hover:underline"
-            >
-              {author}
-            </a>
-            {" | "}
-            {moment(created_at).fromNow()}
-            {" | "}
-            {`${num_comments} comments`}
-          </Typography>
+          <span className="text-xs md:text-base text-gray-400 !font-medium flex items-center gap-1">
+            {points} points by {author} {`| ${moment(created_at).fromNow()}`}
+            {` | ${num_comments || 0} comments`}
+          </span>
         </div>
 
-        <div className="flex items-center gap-4">
-          <Tooltip title="Hide News" placement="left">
-            <DeleteOutlineIcon
-              className="cursor-pointer"
-              onClick={() => hideNews(objectID)}
-            />
+        <div className="flex flex-col items-end gap-4">
+          <Tooltip title="Upvote" placement="left">
+            <ArrowDropUpIcon className="cursor-pointer" />
           </Tooltip>
 
-          {url && (
-            <a href={url}>
-              <Tooltip title={url} placement="bottom-start">
-                <OpenInNewIcon className="cursor-pointer" />
-              </Tooltip>
-            </a>
-          )}
+          <div className="flex items-center gap-4">
+            <Tooltip title="Hide News" placement="left">
+              <DeleteOutlineIcon
+                className="cursor-pointer"
+                onClick={() => hideNews(objectID)}
+              />
+            </Tooltip>
+
+            {url && (
+              <a href={url}>
+                <Tooltip title={url} placement="bottom-start">
+                  <OpenInNewIcon className="cursor-pointer" />
+                </Tooltip>
+              </a>
+            )}
+          </div>
         </div>
       </div>
     </Paper>
